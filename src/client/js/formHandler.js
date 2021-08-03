821ad0df690bc27b02fc231ea581f7a538d8011a
TANADADaisuke <nvkx873110@gmail.com>
2021-08-03 20:03:52 +0900

    feat: Validate form submission

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,23 +5,27 @@ async function handleSubmit(event) {
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    await fetch('http://localhost:8080/test', {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            text: formText
+    if (formText === '') {
+        alert('Form is empty. Please fill in the form!')
+    } else {
+        console.log("::: Form Submitted :::")
+        await fetch('http://localhost:8080/test', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                text: formText
+            })
         })
-    })
-    .then(res => res.json())
-    .then(function(res) {
-        console.log("::: Response :::")
-        console.log(res)
-        document.getElementById('results').innerHTML = res.message
-    })
+        .then(res => res.json())
+        .then(function(res) {
+            console.log("::: Response :::")
+            console.log(res)
+            document.getElementById('results').innerHTML = res.message
+        })
+    }
 }
 
 export { handleSubmit }
